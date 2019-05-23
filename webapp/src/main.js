0df31172a4151c91e75089cb8b29287746e8ae07
zeus <1309170466@qq.com>
2019-05-23 23:51:26 +0800

    [A] add vuex

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import routes from './routes'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
+Vue.use(Vuex)
 
 const router = new VueRouter({
   routes
